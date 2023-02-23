0529b9b49143b0ca21a147ee448c1b17a20eae94
Mike <80423783+MikeTheHash@users.noreply.github.com>
2023-02-23 16:27:54 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install steghide
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install pip
+
+pip install subprocess
+pip install argparse
+pip install colorama
