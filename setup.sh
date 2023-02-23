fccb2cefaa895a430051f1adfd96b7c612fe3953
Mike <80423783+MikeTheHash@users.noreply.github.com>
2023-02-23 16:35:41 +0000

    Adding installer file

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sudo apt install steghide
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install pip
-
-pip install subprocess
-pip install argparse
-pip install colorama
+if [ $USER != "root" ];
+then
+    echo "You need to run this as root"
+elif [ $USER == "root" ];
+then 
+    sudo apt install steghide
+    sudo apt install python3
+    sudo apt install python3-pip
+    sudo apt install pip
+    pip install subprocess
+    pip install argparse
+    pip install colorama
+fi
